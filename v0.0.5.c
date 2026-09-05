6274c36b73441f8ee9726a4288788c41a7dde9c6
yeeeree31 <142909828+yeeeree@users.noreply.github.com>
2026-09-06 07:52:58 +0800

    V0.0.5
    Add a Ghost.

---
+++ v0.0.5.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void move();
+void Ghost_move();
+void array_assign(char a[5][8], char b[5][8], int c, int d);
+
+int pos_x = 1, pos_y = 1, Gpos_x = 5, Gpos_y = 3;
+char Level[5][8] = {{'*','*','*','*','*','*','*','\n'},
+		    {'*',' ',' ',' ',' ',' ','*','\n'},
+		    {'*',' ','*','*','*',' ','*','\n'},
+		    {'*',' ',' ',' ',' ',' ','*','\n'},
+		    {'*','*','*','*','*','*','*','\n'}};
+char Level2[5][8];
+
+int main(){
+	array_assign(Level2, Level, 5, 8); //copy Level
+	for(;;){
+		for(int i = 0; i < 5; i++){
+			for(int j = 0; j < 8; j++){
+				printf("%c", Level[i][j]);
+			}
+		}
+		array_assign(Level, Level2, 5, 8); //refresh map
+		Ghost_move();
+		move();
+		usleep(10*1000); //10ms
+		system("clear");
+	}
+
+	return 0;
+}
+
+void move(){
+	char input;
+	input = getchar();
+
+	if(input == 'w' && Level[pos_x - 1][pos_y] != '*')
+		pos_x--;
+	if(input == 's' && Level[pos_x + 1][pos_y] != '*')
+		pos_x++;
+	if(input == 'a' && Level[pos_x][pos_y - 1] != '*')
+		pos_y--;
+	if(input == 'd' && Level[pos_x][pos_y + 1] != '*')
+		pos_y++;
+	Level[pos_x][pos_y] = 'C';
+}
+
+void array_assign(char a[5][8], char b[5][8], int c, int d){
+	//assign b to a
+	//c is row (horizon)
+	//d is column (vertical)
+	for(int e = 0; e < c; e++){
+		for(int f = 0; f < d; f++){
+			a[e][f] = b[e][f];
+		}
+	}
+}
+
+void Ghost_move(){
+	if(Gpos_x - pos_x > 0 && Level[Gpos_y][Gpos_x - 1] != '*')
+		Gpos_x--;
+	if(Gpos_x - pos_x < 0 && Level[Gpos_y][Gpos_x + 1] != '*')
+		Gpos_x++;
+	if(Gpos_y - pos_y > 0 && Level[Gpos_y - 1][Gpos_x] != '*')
+		Gpos_y--;
+	if(Gpos_y - pos_y < 0 && Level[Gpos_y + 1][Gpos_x] != '*')
+		Gpos_y++;
+	Level[Gpos_y][Gpos_x] = 'M';
+}
